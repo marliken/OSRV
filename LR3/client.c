f6dbdbecd10beb5b9e72b7cd86677efdacc93621
marliken <131689819+marliken@users.noreply.github.com>
2023-04-25 00:33:54 +0300

    Update client.c

--- LR3/client.c
+++ LR3/client.c
@@ -9,14 +9,11 @@ int main()
     char smsg[30] = "Evgeniy Ahmadeev I993";
     char rmsg[200];
     int coid;
-    int chid;
     long serv_pid;
     printf("Prog client , Vvedite PID servera \n");
     scanf("%ld", &serv_pid);
     coid = ConnectAttach(0, serv_pid, 1, 0, 0);
     printf("Connect res %d \n , vvedite soobshenie\n", coid);
-    chid = ChannelCreate(0);
-    printf("Chanel id : %d \n", chid);
     printf("Vveli %s \n", smsg);
     if (MsgSend(coid, smsg, strlen(smsg) + 1, rmsg, sizeof(rmsg)) == -1)
     {
